04de94f50fbe993bccc459668e837f3ca0fcf28e
@hexlet/cli <support@hexlet.io>
2023-10-17 01:15:04 +0300

    submit java-oop-ru/patterns

---
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -0,0 +1,16 @@
+package exercise;
+
+// BEGIN
+public class LabelTag implements TagInterface {
+    String value;
+    TagInterface tag;
+    LabelTag(String value, TagInterface tag) {
+        this.value = value;
+        this.tag = tag;
+    }
+    @Override
+    public String render() {
+        return "<label>" + value + tag.render() + "</label>";
+    }
+}
+// END
