ad416199ad67d7207e171b8e5c616b0311b5ebb5
@hexlet/cli <support@hexlet.io>
2023-11-02 00:25:57 +0300

    submit java-web-ru/mvc

---
+++ java-web-ru/mvc/src/main/java/exercise/dto/posts/EditPostPage.java
@@ -0,0 +1,18 @@
+package exercise.dto.posts;
+
+import java.util.List;
+import java.util.Map;
+import io.javalin.validation.ValidationError;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// BEGIN
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class EditPostPage {
+    String name;
+    String body;
+}
+// END
